b938fe1b7444cdd3b5a2ea577f95ee22cfa93795
nkk36 <nkallfa36@gmail.com>
2017-12-30 16:36:05 -0500

    working app with pre-processing inputs

--- app/server.R
+++ app/server.R
@@ -17,7 +17,6 @@ source("R/Load_Data.R")
 # Load data
 Data = Load_Data(TRUE)
 naics = Data[[1]]
-#rc = Data[[2]]
 vn = Data[[2]]
 
 ###########################################################################################################################
@@ -27,165 +26,229 @@ vn = Data[[2]]
 ###########################################################################################################################
 
 server <- function(input, output){
-  
+
   #################################################################
   #                                                               #
   #                      REACTIVE VALUES                          #
   #                                                               #
   #################################################################
-  
+
   v <- reactiveValues(doPlot = FALSE)
-  
+
   observeEvent(input$update, {
-    # 0 will be coerced to FALSE
-    # 1+ will be coerced to TRUE
     v$doPlot <- input$update
   })
-  
+
   #################################################################
   #                                                               #
   #                       REACTIVE DATA                           #
   #                                                               #
   #################################################################
-  
+
   dataInput <- eventReactive(input$update, {
 
-    # Subset data
-    # naics = naics[rc$ncontract >= input$Min_Contract & 
-    #                 rc$ncontract <= input$Max_Contract |
-    #                 rc$sum >= input$Min_Revenue & 
-    #                 rc$sum <= input$Max_Revenue,]
-    # naics = naics[,2:ncol(naics)] # Remove duns column
+    # Convert to percentage
+    if (input$Perc_Total == "Percent"){
+      naics[,1:ncol(naics)] = naics[,1:ncol(naics)]/sum(naics[,1:ncol(naics)])
+    }
+    
+    if (input$Scale_Variables == TRUE){
+      naics = scale(naics)
+    }
+    
+    if (input$Dim_Reduction == "PCA"){
+      naics = data.frame(prcomp(naics)$x[,1:input$Components])
+    }
+    else if (input$Dim_Reduction == "SVD"){
+      SVD = svd(naics)
+      naics = data.frame(SVD$u[,1:input$Components])
+    }
+    
+        # Take only columns that have a non-zero column sum
+        naics = naics[,which(colSums(naics) != 0)]
+
+    # Return data
+    naics
+  })
+
+  originalData <- eventReactive(input$update, {
 
     # Convert to percentage
     if (input$Perc_Total == "Percent"){
       naics[,1:ncol(naics)] = naics[,1:ncol(naics)]/sum(naics[,1:ncol(naics)])
     }
 
-    # # Take only columns that have a sum greater than 0
-    # naics = naics[,which(colSums(naics) > 0)]
-    
+    if (input$Scale_Variables == TRUE){
+      naics = scale(naics)
+    }
+
+    # Take only columns that have a non-zero column sum
+    naics = naics[,which(colSums(naics) !=0)]
+
     # Return data
     naics
   })
-  
-  duns <- reactive({
 
-    # Subset data
-    # vn = vn[rc$ncontract >= input$Min_Contract & 
-    #           rc$ncontract <= input$Max_Contract | 
-    #           rc$sum >= input$Min_Revenue & 
-    #           rc$sum <= input$Max_Revenue,]
-    
+  duns <- eventReactive(input$update, {
     vn
-
   })
-  
+
   #################################################################
   #                                                               #
   #                         OUTPUTS                               #
   #                                                               #
   #################################################################
-  
-  output$pca_plot <- renderPlot({
+
+  output$Dim_Reduction_Plot = renderPlot({
     if (v$doPlot == FALSE) return()
-    
+
     isolate({
-      autoplot(prcomp(scale(dataInput())))
+
+      if (input$Dim_Reduction == "PCA"){
+        autoplot(prcomp(originalData()))
+      }
+      else if (input$Dim_Reduction == "SVD"){
+        SVD = svd(originalData())
+        SVD = data.frame(SVD$u)
+        ggplot(data = SVD) +
+          geom_point(mapping = aes(x = X1, y = X2)) +
+          ggtitle("Top Two Singular Vectors") +
+          xlab("First Singular Vector") +
+          ylab("Second Singular Vector")
+        }
+
+
     })
 
   }) # End pca_plot
-  
-  output$pca_sum <- renderPlot({
+
+  output$Variance_Explained = renderPlot({
     if (v$doPlot == FALSE) return()
-    
+
     isolate({
-      pca = prcomp(scale(dataInput()))
-      plot(1 - pca$sdev^2/sum(pca$sdev^2), xlab = "Principal Components", ylab = "Variance Explained")
+
+      Components = input$Components
+      if (input$Dim_Reduction == "PCA"){
+        
+        Components = input$Components
+        pca = prcomp(originalData())
+        pca = 1 - pca$sdev^2/sum(pca$sdev^2)
+        pca = data.frame(var_exp = pca[1:Components])
+        
+        ggplot(data = pca) + 
+          geom_point(mapping = aes(x = 1:Components, 
+                                   y = var_exp,
+                                   size = 1)
+                     ) + 
+          scale_x_continuous(breaks = 1:Components) + 
+          scale_y_continuous(limits = c(0,1)) +
+          xlab("Principal Components") + 
+          ylab("Variance Explained") + 
+          theme(legend.position="none")
+      }
+      else if (input$Dim_Reduction == "SVD"){
+        
+        Components = input$Components
+        SVD = svd(originalData())
+        SVD = 1 - SVD$d^2/sum(SVD$d^2)
+        SVD = data.frame(var_exp = SVD[1:Components])
+        
+        ggplot(data = SVD) + 
+          geom_point(mapping = aes(x = 1:Components, 
+                                   y = var_exp,
+                                   size = 1)
+          ) + 
+          scale_x_continuous(breaks = 1:Components) + 
+          scale_y_continuous(limits = c(0,1)) +
+          xlab("Singular Vectors") + 
+          ylab("Variance Explained") + 
+          theme(legend.position="none")
+      }
     })
-    
+
   }) # End pca_sum
-  
+
+
   output$comparison_table <- renderDataTable({
     if (v$doPlot == FALSE) return()
-    
+
     isolate({
-      if (input$Clustering_Method == "K_Means" & input$Similarity_Calc == "Euclidean" & input$Scale_Variables == TRUE){
-        
-        d.kmeans = kmeans(dist(scale(dataInput())), centers = input$Num_Groups)
-        GetGroup = d.kmeans$cluster[which(duns()$duns == input$Vendor)]
-        close = data.frame(duns()[which(d.kmeans$cluster == GetGroup),])
-        close = close[order(close$vendorname),]
-        
-        datatable(close,options = list("pageLength" = 10))
-      }else if (input$Clustering_Method == "K_Means" & input$Similarity_Calc == "Euclidean" & input$Scale_Variables == FALSE){
-        
+      if (input$Clustering_Method == "K_Means" & input$Similarity_Calc == "Euclidean"){
+
         d.kmeans = kmeans(dist(dataInput()), centers = input$Num_Groups)
         GetGroup = d.kmeans$cluster[which(duns()$duns == input$Vendor)]
         close = data.frame(duns()[which(d.kmeans$cluster == GetGroup),])
         close = close[order(close$vendorname),]
-        
+
         datatable(close,options = list("pageLength" = 10))
-      }else if (input$Clustering_Method == "K_Means" & input$Similarity_Calc == "Pearson" & input$Scale_Variables == TRUE){
-        
-        d.kmeans = kmeans(as.dist(1 - cor(t(scale(dataInput())), method = "pearson")), centers = input$Num_Groups)
+      }else if (input$Clustering_Method == "K_Means" & input$Similarity_Calc == "Euclidean"){
+
+        d.kmeans = kmeans(dist(dataInput()), centers = input$Num_Groups)
         GetGroup = d.kmeans$cluster[which(duns()$duns == input$Vendor)]
         close = data.frame(duns()[which(d.kmeans$cluster == GetGroup),])
         close = close[order(close$vendorname),]
-        
+
         datatable(close,options = list("pageLength" = 10))
-      }else if (input$Clustering_Method == "K_Means" & input$Similarity_Calc == "Pearson" & input$Scale_Variables == FALSE){
-        
+      }else if (input$Clustering_Method == "K_Means" & input$Similarity_Calc == "Pearson"){
+
         d.kmeans = kmeans(as.dist(1 - cor(t(dataInput()), method = "pearson")), centers = input$Num_Groups)
         GetGroup = d.kmeans$cluster[which(duns()$duns == input$Vendor)]
         close = data.frame(duns()[which(d.kmeans$cluster == GetGroup),])
         close = close[order(close$vendorname),]
-        
+
         datatable(close,options = list("pageLength" = 10))
-      }else if (input$Clustering_Method == "Heirarchical" & input$Similarity_Calc == "Euclidean" & input$Scale_Variables == TRUE){
-        
-        d.hclust = hclust(dist(scale(dataInput())), method = tolower(input$HClust_Method))
-        cut = cutree(d.hclust, k = input$Num_Groups)
-        GetGroup = cut[which(duns()$duns == input$Vendor)]
-        close = data.frame(duns()[which(cut == GetGroup),])
+      }else if (input$Clustering_Method == "K_Means" & input$Similarity_Calc == "Pearson"){
+
+        d.kmeans = kmeans(as.dist(1 - cor(t(dataInput()), method = "pearson")), centers = input$Num_Groups)
+        GetGroup = d.kmeans$cluster[which(duns()$duns == input$Vendor)]
+        close = data.frame(duns()[which(d.kmeans$cluster == GetGroup),])
         close = close[order(close$vendorname),]
-        
+
         datatable(close,options = list("pageLength" = 10))
-      }else if (input$Clustering_Method == "Heirarchical" & input$Similarity_Calc == "Euclidean" & input$Scale_Variables == FALSE){
-        
+      }else if (input$Clustering_Method == "Heirarchical" & input$Similarity_Calc == "Euclidean"){
+
         d.hclust = hclust(dist(dataInput()), method = tolower(input$HClust_Method))
-        cut = cutree(d.hclust, k = input$Num_Groups)
+        cut = cutree(d.hclust, k = input$HClust_NGroups)
+        GetGroup = cut[which(duns()$duns == input$Vendor)]
+        close = data.frame(duns()[which(cut == GetGroup),])
+        close = close[order(close$vendorname),]
+
+        datatable(close,options = list("pageLength" = 10))
+      }else if (input$Clustering_Method == "Heirarchical" & input$Similarity_Calc == "Euclidean"){
+
+        d.hclust = hclust(dist(dataInput()), method = tolower(input$HClust_Method))
+        cut = cutree(d.hclust, k = input$HClust_NGroups)
         GetGroup = cut[which(duns()$duns == input$Vendor)]
         close = data.frame(duns()[which(cut == GetGroup),])
         close = close[order(close$vendorname),]
         close = close[order(close$vendorname),]
-        
+
         datatable(close,options = list("pageLength" = 10))
-      }else if (input$Clustering_Method == "Heirarchical" & input$Similarity_Calc == "Pearson" & input$Scale_Variables == TRUE){
-        
-        d.hclust = hclust(as.dist(1 - cor(t(scale(dataInput())), method = "pearson")), method = tolower(input$HClust_Method))
-        cut = cutree(d.hclust, k = input$Num_Groups)
-        GetGroup = cut[which(duns()$duns == input$Vendor)]
-        close = data.frame(duns()[which(cut == GetGroup),])
-        close = close[order(close$vendorname),]
-        
-        datatable(close,options = list("pageLength" = 10))
-      }else if (input$Clustering_Method == "Heirarchical" & input$Similarity_Calc == "Pearson" & input$Scale_Variables == FALSE){
-        
+      }else if (input$Clustering_Method == "Heirarchical" & input$Similarity_Calc == "Pearson"){
+
         d.hclust = hclust(as.dist(1 - cor(t(dataInput()), method = "pearson")), method = tolower(input$HClust_Method))
-        cut = cutree(d.hclust, k = input$Num_Groups)
+        cut = cutree(d.hclust, k = input$HClust_NGroups)
+        GetGroup = cut[which(duns()$duns == input$Vendor)]
+        close = data.frame(duns()[which(cut == GetGroup),])
+        close = close[order(close$vendorname),]
+
+        datatable(close,options = list("pageLength" = 10))
+      }else if (input$Clustering_Method == "Heirarchical" & input$Similarity_Calc == "Pearson"){
+
+        d.hclust = hclust(as.dist(1 - cor(t(dataInput()), method = "pearson")), method = tolower(input$HClust_Method))
+        cut = cutree(d.hclust, k = input$HClust_NGroups)
         GetGroup = cut[which(duns()$duns == input$Vendor)]
         close = data.frame(duns()[which(cut == GetGroup),])
         close = close[order(close$vendorname),]
 
         datatable(close,options = list("pageLength" = 10))
       }
-    }) 
+    })
     })# End comparison_table
-  
+
   output$hclust_plot <- renderPlot({
     if (v$doPlot == FALSE) return()
-    
+
     isolate({
       if (input$Similarity_Calc == "Euclidean"){
         d.hclust = hclust(dist(dataInput()), method = tolower(input$HClust_Method))
@@ -199,27 +262,9 @@ server <- function(input, output){
 
       plot(d.hclust, labels = FALSE, xlab = "Companies", sub = "") #+ abline(h = vline, col = "blue")
     })
-    
+
   }) # End hclust_plot
-  
-  
-  
+
+
+
 } # End server
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- app/ui.R
+++ app/ui.R
@@ -22,60 +22,61 @@ ui <- dashboardPage(
       menuItem("Home", tabName = "home", icon = icon("home")),
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
       textInput(inputId = "Vendor", label = "Choose vendor:", value = "964725688"), #77815736
-      menuItem(text = "Filter Inputs", 
+      menuItem(text = "Pre-Process Inputs",
                icon = icon("filter"),
-               sliderInput(inputId = "Min_Contract", label = "Minimum Number of Contracts:", min = 0, max = 100, value = 50, step = 1),
-               sliderInput(inputId = "Max_Contract", label = "Maximum Number of Contracts:", min = 0, max = 1500, value = 100, step = 1),
-               numericInput(inputId = "Min_Revenue", label = "Minimum revenue:", value = 20000000, min = 0, max = NA, step = 1),
-               numericInput(inputId = "Max_Revenue", label = "Maximum revenue:", value = 80000000, min = 0, max = NA, step = 1)
+               selectInput(inputId = "Dim_Reduction",
+                           label = "Dimensionality Reduction:",
+                           choices = c("None", "PCA", "SVD"),
+                           selected = "PCA"),
+               conditionalPanel(condition = "input.Dim_Reduction != 'None'",
+                                selectInput(inputId = "Components",
+                                            label = "Choose number of reduced dimensions (Maximum of 10):",
+                                            choices = 1:10,
+                                            selected = 10,
+                                            multiple = FALSE)
+                                ),
+               checkboxInput(inputId = "Scale_Variables",
+                             label = "Scale Variables",
+                             value = FALSE),
+               selectInput(inputId = "Perc_Total",
+                           label = "Total or Percent:",
+                           choices = c("Total" = "Total", "Percent" = "Percent"),
+                           selected = "Total", multiple = FALSE)
       )
     ),
     sidebarMenu(
       menuItem(text = "Clustering Inputs",
                icon = icon("cube"),
-               selectInput(inputId = "Clustering_Method", 
-                           label = "Choose clustering method:", 
-                           choices = c("K-Means" = "K_Means", "Heirarchical" = "Heirarchical"), 
-                           selected = "K-Means", 
+               selectInput(inputId = "Clustering_Method",
+                           label = "Choose clustering method:",
+                           choices = c("K-Means" = "K_Means", "Heirarchical" = "Heirarchical"),
+                           selected = "K-Means",
                            multiple = FALSE),
-               # sliderInput(inputId = "Num_Groups", 
-               #             label = "Number of Groups:",
-               #             min = 1,
-               #             max = 100,
-               #             value = 1,
-               #             step = 1),
                conditionalPanel(condition = "input.Clustering_Method == 'K_Means'",
                                 sliderInput(inputId = "Num_Groups",
                                             label = "Number of Groups:",
                                             min = 1,
                                             max = 100,
-                                            value = 1,
+                                            value = 10,
                                             step = 1)
                ),
                conditionalPanel(condition = "input.Clustering_Method == 'Heirarchical'",
-                                selectInput(inputId = "HClust_Method", label = "Choose heirarchical clustering method:", 
-                                            choices = c("Complete" = "Complete", "Average" = "Average"), 
-                                            selected = "Average", 
+                                selectInput(inputId = "HClust_Method", label = "Choose heirarchical clustering method:",
+                                            choices = c("Complete" = "Complete", "Average" = "Average"),
+                                            selected = "Average",
                                             multiple = FALSE)
                ),
-               selectInput(inputId = "Similarity_Calc", 
-                           label = "Choose similarity calculation:", 
-                           choices = c("Euclidean" = "Euclidean", "Pearson" = "Pearson"), 
-                           selected = "Pearson", 
-                           multiple = FALSE),
-               checkboxInput(inputId = "Scale_Variables", 
-                             label = "Scale Variables", 
-                             value = FALSE),
-               selectInput(inputId = "Perc_Total", 
-                           label = "Percent or Total:", 
-                           choices = c("Total" = "Total", "Percent" = "Percent"), 
-                           selected = "Total", multiple = FALSE)
+               selectInput(inputId = "Similarity_Calc",
+                           label = "Choose similarity calculation:",
+                           choices = c("Euclidean" = "Euclidean", "Pearson" = "Pearson"),
+                           selected = "Euclidean",
+                           multiple = FALSE)
       )
     ),
     actionButton("update","Update")
   ),
   dashboardBody(
-    
+
     tabItems(
       tabItem(tabName = "home",
               includeMarkdown("intro.md")
@@ -83,16 +84,25 @@ ui <- dashboardPage(
       tabItem(tabName = "dashboard",
               fluidRow(
                 box(DT::dataTableOutput("comparison_table"), width = 6),
-                tabBox(tabPanel(title = "Variance Explained", plotOutput("pca_sum")),
-                       tabPanel(title = "PCA Plot", plotOutput("pca_plot")), id = "pca", selected = "Variance Explained",
-                       title = "Principal Component Analysis (PCA)", width = 6)
+                conditionalPanel(condition = "input.Dim_Reduction != 'None'",
+                                 fluidRow(
+                                   tabBox(tabPanel(title = "Variance Explained",
+                                                   plotOutput("Variance_Explained")),
+                                          tabPanel(title = "Plot",
+                                                   plotOutput("Dim_Reduction_Plot")),
+                                          id = "pca",
+                                          selected = "Variance Explained",
+                                          title = "Dimensionality Reduction",
+                                          width = 6)
+                                   )
+                                 )
                 ),
               conditionalPanel(condition = "input.Clustering_Method == 'Heirarchical'",
                                fluidRow(
-                                 box(sliderInput(inputId = "HClust_NGroups", 
+                                 box(sliderInput(inputId = "HClust_NGroups",
                                                           label = "Number of groups:",
                                                           min = 1,
-                                                          max = 100, 
+                                                          max = 100,
                                                           value = 1), width = 4),
                                  box(plotOutput("hclust_plot"), width = 8)))
       )
